441d68275d90e58957aaa6e0a2d81646d12e0bb8
karthiksalapu <131908602+karthiksalapu@users.noreply.github.com>
2024-05-02 22:27:24 +0530

    Create index.js

---
+++ 01) Dynamic Web Projects /03) Seasons Switcher/index.js
@@ -0,0 +1,26 @@
+let smallDeviceImage = document.getElementById("seasonSmallImage");
+let mediumDeviceImage = document.getElementById("seasonMediumImage");
+
+
+function clickSpring() {
+    smallDeviceImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png"
+    mediumDeviceImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png"
+}
+
+function clickSummer() {
+    smallDeviceImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-xs-img.png"
+    mediumDeviceImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-md-img.png"
+
+}
+
+function clickAutumn() {
+    smallDeviceImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-xs-img.png"
+    mediumDeviceImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-md-img.png"
+
+}
+
+function clickWinter() {
+    smallDeviceImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-xs-img.png"
+    mediumDeviceImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-md-img.png"
+
+}
