73ac7b3d52294a558abfc589bec3452318d4178b
karthiksalapu <131908602+karthiksalapu@users.noreply.github.com>
2024-06-10 22:31:23 +0530

    Create index.js

---
+++ 01) Dynamic Web Projects /30) Clear the Counter Timer/index.js
@@ -0,0 +1,15 @@
+let clearBtnEl = document.getElementById("clearBtn");
+let counterValue = document.getElementById("counterValue");
+
+let counter = 0;
+
+let counterTimer = function() {
+    counter = counter + 1;
+    counterValue.textContent = counter;
+};
+
+let intervalId = setInterval(counterTimer, 1000);
+
+clearBtnEl.onclick = function() {
+    clearInterval(intervalId);
+};
