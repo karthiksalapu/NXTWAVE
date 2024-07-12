99db9a6490ae79f3070aef298610f3d5ba30b6dc
karthiksalapu <131908602+karthiksalapu@users.noreply.github.com>
2024-07-12 23:32:03 +0530

    Create index.js

---
+++ 01) Dynamic Web Projects /37.3) HTTP Put Method Practice/index.js
@@ -0,0 +1,42 @@
+let idText = document.getElementById("userInput");
+let TextAreaInput = document.getElementById("requestBody");
+let ButtonEl = document.getElementById("sendPutRequestBtn");
+let StatusText = document.getElementById("requestStatus");
+let ResponseText = document.getElementById("httpResponse");
+let loadingEl = document.getElementById("loading");
+
+
+function reqAndRespond() {
+    loadingEl.classList.remove("d-none");
+    StatusText.classList.add("d-none");
+
+    let dataInput = TextAreaInput.value;
+    let IdValue = idText.value;
+    let url = "https://gorest.co.in/public-api/users/" + IdValue;
+    let options = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: "Bearer 7d97e27f1fc18a4aa24120f42c6bc25d0e3d59434bde21fd1169f20277014faf"
+        },
+        body: dataInput
+    };
+
+    fetch(url, options)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            loadingEl.classList.add("d-none");
+            StatusText.classList.remove("d-none");
+
+            StatusText.textContent = data.code;
+            ResponseText.textContent = JSON.stringify(data);
+        });
+}
+
+
+ButtonEl.onclick = function() {
+    reqAndRespond();
+};
