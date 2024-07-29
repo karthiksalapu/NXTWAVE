4bc2e36174ee51fb81821e7e380aaa386bbb1aff
karthiksalapu <131908602+karthiksalapu@users.noreply.github.com>
2024-07-29 23:21:01 +0530

    Create index.js

---
+++ 01) Dynamic Web Projects /45) select your pet/index.js
@@ -0,0 +1,18 @@
+let petsImageUrls = {
+    dog: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-dog-img.png",
+    cat: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-cat-img.png",
+    parrot: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-parrot-img.png",
+    spider: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-spider-img.png",
+    rabbit: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-rabbit-img.png"
+};
+
+let petSelect = document.getElementById("petSelect");
+let petImg = document.getElementById("petImg");
+
+function ImageChange(event) {
+    let ItemName = event.target.value;
+    petImg.src = petsImageUrls[ItemName];
+    petImg.classList.add("w-100");
+}
+
+petSelect.addEventListener("change", ImageChange);
