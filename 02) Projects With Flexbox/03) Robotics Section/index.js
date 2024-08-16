1a6b747ebe30cf67b8a286c8e645976668d4b9c7
karthiksalapu <131908602+karthiksalapu@users.noreply.github.com>
2024-08-16 23:43:36 +0530

    Create index.js

---
+++ 02) Projects With Flexbox/03) Robotics Section/index.js
@@ -0,0 +1,25 @@
+Robotics Section
+Solved
+Easy
+
+Helpful
+In this assignment, let's build a Robotics Section by applying the CSS concepts we learned till now.
+
+Refer to the below image.
+Extra Large (Size >= 1200px)
+https://assets.ccbp.in/frontend/content/intermediate-rwd/robotics-output-v1.png
+
+about_robotics
+
+Achieve the design for the devices whose width is greater than or equal to1200px
+
+Resources
+Use the image URL's given below.
+
+https://assets.ccbp.in/frontend/intermediate-rwd/robotics-bg.png
+https://assets.ccbp.in/frontend/intermediate-rwd/robotics-img.png
+CSS Colors used:
+#1e3a8a
+#475569
+CSS Font families used:
+Roboto
